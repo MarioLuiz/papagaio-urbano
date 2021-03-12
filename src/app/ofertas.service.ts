cf089531b4d9b0bbb12a94174e6323e1b6b8d849
MarioLuiz <marioluiz.arruda@gmail.com>
2021-03-11 23:18:51 -0400

    Adicionando Promise para simular um ambiente com api rest com dalay de resposta

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -11,10 +11,10 @@ export class OfertasService {
             valor: 29.90,
             destaque: true,
             imagens: [
-                {url: "/assets/ofertas/1/img1.jpg"},
-                {url: "/assets/ofertas/1/img2.jpg"},
-                {url: "/assets/ofertas/1/img3.jpg"},
-                {url: "/assets/ofertas/1/img4.jpg"}
+                { url: "/assets/ofertas/1/img1.jpg" },
+                { url: "/assets/ofertas/1/img2.jpg" },
+                { url: "/assets/ofertas/1/img3.jpg" },
+                { url: "/assets/ofertas/1/img4.jpg" }
             ]
         },
         {
@@ -26,12 +26,12 @@ export class OfertasService {
             valor: 32.90,
             destaque: true,
             imagens: [
-                {url: "/assets/ofertas/2/img1.jpg"},
-                {url: "/assets/ofertas/2/img2.jpg"},
-                {url: "/assets/ofertas/2/img3.jpg"},
-                {url: "/assets/ofertas/2/img4.jpg"}
+                { url: "/assets/ofertas/2/img1.jpg" },
+                { url: "/assets/ofertas/2/img2.jpg" },
+                { url: "/assets/ofertas/2/img3.jpg" },
+                { url: "/assets/ofertas/2/img4.jpg" }
             ]
-        
+
         },
         {
             id: 4,
@@ -42,16 +42,47 @@ export class OfertasService {
             valor: 31.90,
             destaque: true,
             imagens: [
-                {url: "/assets/ofertas/3/img1.jpg"},
-                {url: "/assets/ofertas/3/img2.jpg"},
-                {url: "/assets/ofertas/3/img3.jpg"},
-                {url: "/assets/ofertas/3/img4.jpg"},
-                {url: "/assets/ofertas/3/img5.jpg"},
-                {url: "/assets/ofertas/3/img6.jpg"}
+                { url: "/assets/ofertas/3/img1.jpg" },
+                { url: "/assets/ofertas/3/img2.jpg" },
+                { url: "/assets/ofertas/3/img3.jpg" },
+                { url: "/assets/ofertas/3/img4.jpg" },
+                { url: "/assets/ofertas/3/img5.jpg" },
+                { url: "/assets/ofertas/3/img6.jpg" }
             ]
         }
     ]
+
     public getOfertas(): Array<Oferta> {
         return this.ofertas
+    }
+
+    public getOfertas2(): Promise<Oferta[]> {
+        return new Promise((resolve, reject) => {
+            //algum processamento, que ao finalizar, chama a função resolve ou a função reject
+            let deu_certo = true
+            if (deu_certo) {
+                setTimeout(() => resolve(this.ofertas), 3000)
+            } else {
+                reject({ codigo_erro: 404, mensagem_erro: 'Servidor não encontrado' })
+            }
+        })
+        .then(( ofertas: any) => {
+            console.log('Primero then', ofertas)
+            return ofertas
+        })
+        .then(( ofertas: any) => {
+            console.log('Segundo then', ofertas)
+            return ofertas
+        })
+        .then(( ofertas: any) => {
+            console.log('Terceiro then', ofertas)
+            return new Promise((resolve2, reject2) => {
+                setTimeout(() => resolve2(ofertas), 3000)
+            })
+        })
+        .then((ofertas: any ) => {
+            console.log('Quarto then executado após 3 segundos por que estava aguardando uma promisse ser resolvida')
+            return ofertas
+        })
     }
 }
