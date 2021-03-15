e45294335dda90c65ada3f41bcaf91e9620a1b66
MarioLuiz <marioluiz.arruda@gmail.com>
2021-03-14 22:36:29 -0400

    Adicionado argumento para trazer oferta em destaques e melhorado layout das ofertas

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -2,12 +2,10 @@ import { Oferta } from "./shared/oferta.model"
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from "@angular/core"
 
-//import 'rxjs/add/operator/toPromise'
-
 @Injectable()
 export class OfertasService {
 
-    url: string = 'http://localhost:3000/ofertas'
+    url: string = 'http://localhost:3000/ofertas?destaque=true'
 
     constructor(private http: HttpClient) { }
 
