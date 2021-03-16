d9ac611efc9e2d5b0d63a605ce2c54168bea3f42
MarioLuiz <marioluiz.arruda@gmail.com>
2021-03-15 21:40:58 -0400

    Solicitando ofertas do restaurante da api e exibindo dinamicamente

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -5,12 +5,18 @@ import { Injectable } from "@angular/core"
 @Injectable()
 export class OfertasService {
 
-    url: string = 'http://localhost:3000/ofertas?destaque=true'
+    urlTrazDestaques: string = 'http://localhost:3000/ofertas?destaque=true'
+    urlFiltraCategoria: string = 'http://localhost:3000/ofertas?categoria='
 
     constructor(private http: HttpClient) { }
 
     public getOfertas(): Promise<Array<Oferta>> {
-        return this.http.get(this.url).toPromise()
+        return this.http.get(this.urlTrazDestaques).toPromise()
+            .then((resposta: any) => resposta)
+    }
+
+    public getOfertasPorCategoria(categoria: string): Promise<Array<Oferta>> {
+        return this.http.get(`${this.urlFiltraCategoria}${categoria}`).toPromise()
             .then((resposta: any) => resposta)
     }
 }
--- src/app/restaurantes/restaurantes.component.ts
+++ src/app/restaurantes/restaurantes.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../shared/oferta.model'
 
 @Component({
   selector: 'app-restaurantes',
   templateUrl: './restaurantes.component.html',
-  styleUrls: ['./restaurantes.component.css']
+  styleUrls: ['./restaurantes.component.css'],
+  providers: [OfertasService]
 })
+
 export class RestaurantesComponent implements OnInit {
 
-  constructor() { }
+  public ofertas: Oferta[] = [];
+
+  constructor(private ofertasService: OfertasService) { }
 
   ngOnInit(): void {
+    this.ofertasService.getOfertasPorCategoria('restaurante')
+      .then((ofertas: Oferta[]) => {
+        console.log('Ofertas:', ofertas)
+        this.ofertas = ofertas
+      })
   }
 
 }
