c0017975adc449480381a42946fb596ed122cd7a
MarioLuiz <marioluiz.arruda@gmail.com>
2021-03-16 21:28:10 -0400

    Isolando endereço url da api e importando no service

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,3 +1,4 @@
+import { URL_API } from './app.api';
 import { Oferta } from "./shared/oferta.model"
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from "@angular/core"
@@ -5,24 +6,22 @@ import { Injectable } from "@angular/core"
 @Injectable()
 export class OfertasService {
 
-    urlTrazDestaques: string = 'http://localhost:3000/ofertas?destaque=true'
-    urlFiltraCategoria: string = 'http://localhost:3000/ofertas?categoria='
-    urlFiltraId: string = 'http://localhost:3000/ofertas?id='
+    //private url_api = 'http://localhost:3000/ofertas'
 
     constructor(private http: HttpClient) { }
 
     public getOfertas(): Promise<Array<Oferta>> {
-        return this.http.get(this.urlTrazDestaques).toPromise()
+        return this.http.get(`${URL_API}?destaque=true`).toPromise()
             .then((resposta: any) => resposta)
     }
 
     public getOfertasPorCategoria(categoria: string): Promise<Array<Oferta>> {
-        return this.http.get(`${this.urlFiltraCategoria}${categoria}`).toPromise()
+        return this.http.get(`${URL_API}?categoria=${categoria}`).toPromise()
             .then((resposta: any) => resposta)
     }
 
     public getOfertaPorId(id: number): Promise<Oferta> {
-        return this.http.get(`${this.urlFiltraId}${id}`).toPromise()
+        return this.http.get(`${URL_API}?id=${id}`).toPromise()
             .then((resposta: any) => {
                 return resposta[0]
             })
