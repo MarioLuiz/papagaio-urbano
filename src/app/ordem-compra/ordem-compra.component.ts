4a5022ef77243943c9fb7486aefbf25fffcd524b
MarioLuiz <marioluiz.arruda@gmail.com>
2021-04-06 19:23:32 -0400

    Adicionado variaveis com Two-Way-Binding na ordemCompra

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -3,13 +3,33 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ordem-compra',
   templateUrl: './ordem-compra.component.html',
-  styleUrls: ['./ordem-compra.component.css']
+  styleUrls: ['./ordem-compra.component.css'],
 })
 export class OrdemCompraComponent implements OnInit {
 
+  public endereco: string = '';
+  public numero: string = '';
+  public complemento: string = '';
+  public formaPagamento: string = '';
+
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
+  atualizaEndereco(endereco: string): void {
+    this.endereco = endereco;
+    console.log(this.endereco)
+  }
+  atualizaNumero(numero: string): void {
+    this.numero = numero;
+    console.log(this.numero)
+  }
+  atualizaComplemento(complemento: string): void {
+    this.complemento = complemento;
+    console.log(this.complemento)
+  }
+  atualizaFormaPagamento(formaPagamento: string): void {
+    this.formaPagamento = formaPagamento;
+    console.log(this.formaPagamento)
+  }
 }
