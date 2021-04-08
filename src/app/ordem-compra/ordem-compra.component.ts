320235e167e00b61809f8b7b743f8be0db712e36
MarioLuiz <marioluiz.arruda@gmail.com>
2021-04-07 21:35:42 -0400

    Adicionado validação hard-Code nos Dados de entrega e Dados de pagamento

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -12,24 +12,78 @@ export class OrdemCompraComponent implements OnInit {
   public complemento: string = '';
   public formaPagamento: string = '';
 
+  // Controles de validação
+  public enderecoValido: boolean | undefined
+  public numeroValido: boolean | undefined
+  public complementoValido: boolean | undefined
+  public formaPagamentoValido: boolean | undefined
+
+  // estados primitivos dos campos (prestine)
+  public enderecoEstadoPrimitivo: boolean = true
+  public numeroEstadoPrimitivo: boolean = true
+  public complementoEstadoPrimitivo: boolean = true
+  public formaPagamentoEstadoPrimitivo: boolean = true
+
+  //controlar botão confirmar Compra
+  public formEstado: string = 'disable';
+
   constructor() { }
 
   ngOnInit(): void { }
 
   atualizaEndereco(endereco: string): void {
     this.endereco = endereco;
-    console.log(this.endereco)
+    //console.log(this.endereco)
+    this.enderecoEstadoPrimitivo = false
+    if (endereco.length > 3) {
+      this.enderecoValido = true;
+    } else {
+      this.enderecoValido = false;
+    }
+    this.habilitaForm()
   }
+
   atualizaNumero(numero: string): void {
     this.numero = numero;
-    console.log(this.numero)
+    //console.log(this.numero)
+    this.numeroEstadoPrimitivo = false
+    if (Number.parseInt(numero) > 0) {
+      this.numeroValido = true;
+    } else {
+      this.numeroValido = false;
+    }
+    this.habilitaForm()
   }
+
   atualizaComplemento(complemento: string): void {
     this.complemento = complemento;
-    console.log(this.complemento)
+    //console.log(this.complemento)
+    this.complementoEstadoPrimitivo = false
+    if (complemento) {
+      this.complementoValido = true;
+    } else {
+      this.complementoValido = false;
+    }
   }
+
   atualizaFormaPagamento(formaPagamento: string): void {
     this.formaPagamento = formaPagamento;
-    console.log(this.formaPagamento)
+    this.formaPagamentoEstadoPrimitivo = false
+    //console.log(this.formaPagamento)
+    this.formaPagamentoEstadoPrimitivo = false
+    if (formaPagamento) {
+      this.formaPagamentoValido = true;
+    } else {
+      this.formaPagamentoValido = false;
+    }
+    this.habilitaForm()
+  }
+
+  habilitaForm(): void {
+    if (this.enderecoValido && this.numeroValido && this.formaPagamentoValido) {
+      this.formEstado = ''
+    } else {
+      this.formEstado = 'disable'
+    }
   }
 }
