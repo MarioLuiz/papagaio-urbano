728537798e13f2e24e352705860d3b4d1d28a8f8
MarioLuiz <marioluiz.arruda@gmail.com>
2021-04-20 18:28:29 -0400

    Limpando View e classe ordem-compra

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { Pedido } from '../shared/pedido.model';
-import { OrdemCompraService } from './../ordem-compra.service';
+import { Component, OnInit } from '@angular/core';
+import { OrdemCompraService } from '../ordem-compra.service'
+import { Pedido } from '../shared/pedido.model'
 
 @Component({
   selector: 'app-ordem-compra',
@@ -11,33 +10,12 @@ import { OrdemCompraService } from './../ordem-compra.service';
 })
 export class OrdemCompraComponent implements OnInit {
 
-  //Pedido
-  public idPedidoCompra: number | undefined;
-
-  @ViewChild('formulario') public formulario: NgForm | undefined;;
-
   constructor(private ordemCompraService: OrdemCompraService) { }
 
-  ngOnInit(): void {
-    //this.ordemCompraService.efetiverCompra()
+  ngOnInit() {
+
   }
 
-  confirmarCompra(): void {
-
-    console.log('Formulario: ', this.formulario)
-    if (this.formulario?.valid) {
-      let pedido: Pedido = new Pedido(
-        this.formulario?.value.endereco,
-        this.formulario?.value.numero,
-        this.formulario?.value.complemento,
-        this.formulario?.value.formaPagamento
-      );
-
-      this.ordemCompraService.efetiverCompra(pedido)
-        .subscribe((idPedido: number) => {
-          this.idPedidoCompra = idPedido
-          console.log('Pedido cadastrado com sucesso, ID: ', idPedido)
-        })
-    }
+  public confirmarCompra(): void {
   }
 }
