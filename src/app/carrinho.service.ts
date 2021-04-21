dc044ded688816eec2bbbe8ba26ce22bc4c3c770
MarioLuiz <marioluiz.arruda@gmail.com>
2021-04-21 18:30:58 -0400

    Enviando oferta para o serviço Carrinho

--- src/app/carrinho.service.ts
+++ src/app/carrinho.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from "@angular/core"
 import { ItemCarrinho } from "./shared/item-carrinho.model"
+import { Oferta } from "./shared/oferta.model"
 
 @Injectable()
 class CarrinhoService {
     public itens: ItemCarrinho[] = []
+
+    public exibirItens(): ItemCarrinho[] {
+        return this.itens
+    }
+
+    public incluirItem(oferta: Oferta | undefined): void {
+        console.log('Oferta recebida no servico carrinho: ', oferta)
+        let itemCarrinho:ItemCarrinho = new ItemCarrinho(
+            oferta?.id,
+            oferta?.imagens[0],
+            oferta?.titulo,
+            oferta?.descricao_oferta,
+            oferta?.valor,
+            1
+        )
+        console.log('itemCarrinho: ', itemCarrinho)
+    }
 }
 
 export default CarrinhoService
--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -3,62 +3,41 @@ import { ActivatedRoute, Params } from '@angular/router';
 import { Oferta } from '../shared/oferta.model';
 import { OfertasService } from './../ofertas.service';
 import { interval, Observable, Observer, Subscription } from 'rxjs'
+import CarrinhoService from '../carrinho.service';
 
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
   styleUrls: ['./oferta.component.css'],
-  providers: [OfertasService]
+  providers: [OfertasService, CarrinhoService]
 })
 export class OfertaComponent implements OnInit, OnDestroy {
 
-  // Teste de Observer
-  /* 
-  private tempoObservableSubscription: Subscription = new Subscription;
-  private meuObservableTesteSubscription: Subscription = new Subscription; 
-  */
-  public oferta: Oferta | undefined
+  public oferta: Oferta | undefined = undefined
 
-  constructor(private route: ActivatedRoute,
-    private ofertasService: OfertasService) {
+  constructor(
+    private route: ActivatedRoute,
+    private ofertasService: OfertasService,
+    private carrinhoService: CarrinhoService
+  ) {
   }
 
   ngOnInit(): void {
-    this.route.params.subscribe((parametros:Params) => {
+    this.route.params.subscribe((parametros: Params) => {
       this.ofertasService.getOfertaPorId(parametros.id)
-      .then((oferta: Oferta) => {
-        this.oferta = oferta
-      })
+        .then((oferta: Oferta) => {
+          this.oferta = oferta
+        })
     })
-    // Teste de Observer
-    /* 
-    let tempo = interval(500)
-    this.tempoObservableSubscription = tempo.subscribe((resposta:number)=>console.log(resposta))
-
-    //observable (observavél)
-    let meuObservableTeste = new Observable((observer : Observer<number>) => {
-      observer.next(10)
-      observer.next(15)
-      observer.next(20)
-      observer.next(25)
-      observer.complete()
-      //observer.error('algum erro foi encontrado na stream de eventos')
-    });
-
-    //observable (observador)
-    this.meuObservableTesteSubscription = meuObservableTeste.subscribe(
-      (resultado: number) => console.log('Resultado', resultado + 1),
-      (erro: string) => console.log('Erro: ', erro),
-      () => console.log('Stream de eventos foi finalizada')
-    ) */
+    //console.log('Itens do carrinho: ', this.carrinhoService.exibirItens())
   }
 
   ngOnDestroy() {
-    // Teste de Observer
-    /* 
-    this.meuObservableTesteSubscription.unsubscribe()
-    this.tempoObservableSubscription.unsubscribe()
-    */
+  }
+
+  adicionarItemCarrinho(): void {
+    //console.log('Oferta Adicionada no carrinho: ', this.oferta)
+    this.carrinhoService.incluirItem(this.oferta)
   }
 
 }
